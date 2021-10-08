5890b21da63e165bc2663be0c158ae94fec5aa3e
clmo <clintmogoai@gmail.com>
2021-10-08 15:15:32 +1100

    PORTFOLIO WORK PAGE

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log("clint mogoai")
+var 
+  $("topNav").on("hover", function(){
+    $(".topNav").topNav("active");
+}
+$("quickLinks").on("hover", function(){
+  $(".quickLinks").quickLinks("active");
+}
+$("socials").on("hover", function(){
+  $(".socials").socials("active");
+}
+$("#seeMore").on("click", function(){
+  $("this").find ("#seeMore)("active");
+}
+$("conDeets").on("hover", function(){
+  $("#conDeets").socials("active");
+}
+$("li").on("hover", function(){
+  $(".li").li("active");
+}
+});
